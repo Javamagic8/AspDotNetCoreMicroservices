4cd745a1889efb2675e8579d73bfde71d4d91929
Javamagic8 <74010882+Javamagic8@users.noreply.github.com>
2022-11-30 16:23:18 +0500

    Add product service

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase("DatabaseSettings:DatabaseName");
+            Products = database.GetCollection<Product>("DatabaseSettings:CollectionName");
+            CatalogContextSeed.Seed(Products);
+        }
+    }
+}

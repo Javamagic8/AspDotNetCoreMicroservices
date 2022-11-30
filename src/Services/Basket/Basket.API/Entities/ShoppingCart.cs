e45914188624b80ba6cfa6955818a599d0c629cc
Javamagic8 <74010882+Javamagic8@users.noreply.github.com>
2022-11-30 17:07:49 +0500

    Add basket project

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCart> Items { get; set; }
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            this.UserName = userName;
+        }
+    }
+}
